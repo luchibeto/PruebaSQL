39329a1e52e490811ae22f3bdf4adc77ab68c766
Lucy Tobias Escudero <LTobiasE@pslcol.com.co>
2017-10-17 17:45:48 -0500

    Creacion de prueba de concepto

---
+++ index.js
@@ -0,0 +1,13 @@
+const sequelize = new Sequelize('prueba', 'sa', 'Junio2017++', {
+  host: 'localhost',
+  dialect: 'mysql'|'sqlite'|'postgres'|'mssql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: 'path/to/database.sqlite'
+});
